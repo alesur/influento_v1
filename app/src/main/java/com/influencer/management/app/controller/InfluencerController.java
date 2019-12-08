3e2b3b310c6dd6d8c26b27e36e0ff2b857305d8a
alesur <aleksandr.surkov@outlook.com>
2019-12-08 11:59:49 +0200

    Personal Info web view

--- app/src/main/java/com/influencer/management/app/controller/InfluencerController.java
+++ app/src/main/java/com/influencer/management/app/controller/InfluencerController.java
@@ -1,13 +1,16 @@
 package com.influencer.management.app.controller;
 
 import com.influencer.management.app.model.dao.InfluencerRepository;
+import com.influencer.management.app.model.entity.Influencer;
+import com.influencer.management.app.model.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//testing Juan
+import org.springframework.web.bind.annotation.PathVariable;
+
 @Controller
-public class HomeController {
+public class InfluencerController {
 
     @Autowired
     private InfluencerRepository influencerRepository;
@@ -18,5 +21,16 @@ public class HomeController {
 
         return "index.html";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+
+        Influencer influencer = influencerRepository.getOne(id);
+
+        model.addAttribute("influencer", influencer);
+
+        return "influencer-edit.html";
+    }
+
 
 }
