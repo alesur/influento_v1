4c2bdf444739ca81771f8cdeaf25846691edffea
alesur <aleksandr.surkov@outlook.com>
2019-12-11 00:08:42 +0200

    Active data on dashbord 3 elements

--- app/src/main/java/com/influencer/management/app/controller/HomeController.java
+++ app/src/main/java/com/influencer/management/app/controller/HomeController.java
@@ -1,11 +1,26 @@
 package com.influencer.management.app.controller;
 
+import com.influencer.management.app.model.dao.InfluencerRepository;
+import com.influencer.management.app.model.dao.ProductRepository;
+import com.influencer.management.app.model.dao.ReviewRepository;
+import com.influencer.management.app.model.entity.Influencer;
+import com.influencer.management.app.model.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class HomeController {
+    @Autowired
+    private InfluencerRepository influencerRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @GetMapping("/")
     public String home() {
@@ -14,7 +29,15 @@ public class HomeController {
     }
 
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+
+        long totalInfluencers = influencerRepository.count();
+        long totalReviews = reviewRepository.count();
+        long totalProducts = productRepository.count();
+
+        model.addAttribute("totalInfluencers", totalInfluencers);
+        model.addAttribute("totalReviews", totalReviews);
+        model.addAttribute("totalProducts", totalProducts);
 
         return "dashboard.html";
     }
