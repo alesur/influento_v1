4831fbde9c1235fe756bad942d208f1be611465a
alesur <aleksandr.surkov@outlook.com>
2019-12-14 23:05:41 +0200

    Main page set to dashboard

--- app/src/main/java/com/influencer/management/app/controller/HomeController.java
+++ app/src/main/java/com/influencer/management/app/controller/HomeController.java
@@ -3,12 +3,17 @@ package com.influencer.management.app.controller;
 import com.influencer.management.app.model.dao.InfluencerRepository;
 import com.influencer.management.app.model.dao.ProductRepository;
 import com.influencer.management.app.model.dao.ReviewRepository;
+import com.influencer.management.app.model.entity.DayContacted;
+import com.influencer.management.app.model.entity.Influencer;
+import com.influencer.management.app.model.entity.Review;
 import com.influencer.management.app.service.Owner;
 import com.influencer.management.app.service.RestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -24,13 +29,11 @@ public class HomeController {
     @Autowired
     private ProductRepository productRepository;
 
-    @Autowired
-    private RestService restService;
 
     @GetMapping("/")
     public String home() {
 
-        return "redirect:/influencer";
+        return "redirect:/dashboard";
     }
 
     @GetMapping("/error")
@@ -42,14 +45,19 @@ public class HomeController {
     @GetMapping("/dashboard")
     public String dashboard(Model model) {
 
+        List<Influencer> influencers = influencerRepository.findAll();
+        List<Review> reviews = reviewRepository.findAll();
         long totalInfluencers = influencerRepository.count();
         long totalReviews = reviewRepository.count();
         long totalProducts = productRepository.count();
 
 
+
         model.addAttribute("totalInfluencers", totalInfluencers);
         model.addAttribute("totalReviews", totalReviews);
         model.addAttribute("totalProducts", totalProducts);
+        model.addAttribute("influencer", influencers);
+        model.addAttribute("reviews", reviews);
 
         return "dashboard.html";
     }
