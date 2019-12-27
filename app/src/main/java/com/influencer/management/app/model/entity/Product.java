36c0e52b4004ab3c6ee83d3103befebf453dcba4
alesur <aleksandr.surkov@outlook.com>
2019-12-27 15:56:54 +0200

    make it better

--- app/src/main/java/com/influencer/management/app/model/entity/Product.java
+++ app/src/main/java/com/influencer/management/app/model/entity/Product.java
@@ -37,12 +37,12 @@ public class Product {
     private String link;
 
 
-    @ManyToMany(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
-    @JoinTable(name = "products_influencer",
-    joinColumns = @JoinColumn(name = "product_id"),
-    inverseJoinColumns = @JoinColumn(name = "influencer_id"))
-    private List<Influencer> influencers;
-
+//    @ManyToMany(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+//    @JoinTable(name = "products_influencer",
+//    joinColumns = @JoinColumn(name = "product_id"),
+//    inverseJoinColumns = @JoinColumn(name = "influencer_id"))
+//    private List<Influencer> influencers;
+//
     @Column(name = "created_at", updatable = false)
     @CreationTimestamp
     private Date createdAt;
